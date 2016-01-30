6f5bbf4ad9b9d880b20e63a409b8380219fd3f0b
Karen Quan <quan.karen@gmail.com>
2016-01-30 12:33:22 -0800

    add dynamic cell creation in cells.js, add custom pokemon colors

---
+++ scripts/cells.js
@@ -0,0 +1,11 @@
+var $cells = $([
+  {
+    id: 0,
+    name: 'Pikachu',
+    city: 'Pallet Town',
+    value: 100,
+    color: '#f4d77c',
+    type: 'Property',
+    image: 'pikachu.png'
+  }
+]);
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,34 @@
 var Main = (function() {
 
-  var _init = function() {
-    console.log("herro from main");
-    Objects.init();
+  /* DATA RETRIEVAL ------------------
+     Parse xml file to get data to populate cells
+  */
+  function populateCells() {
+    var $cell, $name, $city, $image, $value;
+    $cells.each(function(cell) {
+      $cell = $('<div />', { 'class': this.type.toLowerCase() + '-cell', css: { 'border-top': '15px solid ' + this.color } });
+      $name = $('<span />', { 'class': 'name', text: this.name });
+      $city = $('<span />', { 'class': 'city', text: this.city });
+      $image = $('<img />', { src: 'images/' + this.image });
+      $value = $('<span />', { 'class': 'value', text: '$' +this.value });
+      $cell.append($image).append($name).append($city).append($value);
+      $('.column-1 div:first-child').append($cell);
+      console.log($cell);
+    });
   }
+
+  function _init() {
+    console.log("herro from main");
+    populateCells();
+  }
+
+  /* GLOBAL VARIABLES ------------------
+
+  */
+  var $board = $('#board');
+  var $column1 = $('.column-1');
+  var $column2 = $('.column-2');
+  var $column3 = $('.column-3');
 
   return {
     init: _init
